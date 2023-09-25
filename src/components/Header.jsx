ab455cc7c3d95d21ebc752c9c6fad2aeade4f793
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2023-09-25 15:11:01 +0100

    classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,15 @@ export default function () {
   return (
     <header>
       <nav>
-        <img src={trollFace} width="32px" height="27px" />
-        <h3>Meme Generator</h3>
-        <h4>React Course - Project 3</h4>
+        <img
+          src={trollFace}
+          width="32px"
+          height="27px"
+          className="header--image"
+        />
+
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">React Course - Project 3</h4>
       </nav>
     </header>
   );
