aec2d66472a9d83eed4a15f25bb0b7107786008f
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2023-09-25 14:30:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meme Generator
+
+Building a Meme Generator making use of Event Listerners, States, Conditional Rendering, Forms, etc in React
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import trollFace from "../assets/troll-face.png";
+
+export default function () {
+  return (
+    <div className="navbar">
+      <nav>
+        <img src={trollFace} width="32px" height="27px" />
+        <h3>Meme Generator</h3>
+        <h4>React Course - Project 3</h4>
+      </nav>
+    </div>
+  );
+}
