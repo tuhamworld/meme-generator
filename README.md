7ec702cf1e8a7e488d593c3654885f75f052b556
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2024-03-07 15:01:23 +0100

    chore: everything else

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Meme Generator
 
 This web application allows you to create hilarious memes with ease. Express your creativity by combining images and text to generate memes that will leave your friends laughing.
+
+## Concepts used
+- Event listeners
+- State
+- Side Effects
+- Conditional rendering
+- React Forms
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -9,11 +9,35 @@ const Meme = () => {
 
   const [allMemeImages, setAllMemeImages] = useState([]);
 
+   /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
+  
+
+  // Using Promise ------------------------------------------------
+  // useEffect(() => {
+  //   fetch("https://api.imgflip.com/get_memes")
+  //     .then((res) => res.json())
+  //     .then((data) => setAllMemeImages(data.data.memes));
+  // }, [allMemeImages]);
+
+
+  // Using Async ------------------------------------
+
   useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then((res) => res.json())
-      .then((data) => setAllMemeImages(data.data.memes));
-  }, [allMemeImages]);
+    const getAllMemes = async ()=> {
+        const res = await fetch("https://api.imgflip.com/get_memes")
+        const data = await res.json()
+        setAllMemeImages(data.data.memes)
+    }
+    getAllMemes()
+}, [])
 
   function showImageUrl() {
     const randomNumber = Math.floor(Math.random() * allMemeImages.length);
@@ -39,7 +63,7 @@ const Meme = () => {
         <input
           type="text"
           className="first--text"
-          placeholder="Shut up"
+          placeholder="When I do"
           name="topText"
           value={meme.topText}
           onChange={handleChange}
@@ -48,7 +72,7 @@ const Meme = () => {
         <input
           type="text"
           className="last--text"
-          placeholder="and take my money"
+          placeholder="they're funny"
           name="bottomText"
           value={meme.bottomText}
           onChange={handleChange}
